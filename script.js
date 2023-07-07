f3a5754b67d9d6bce6bcecf6fd5dd1372018011e
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-07-07 15:16:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
-const input = document.querySelector('input[type="text"]');
-const resultDiv = document.querySelector('#result');
-
-input.addEventListener('change', () => {
-  const decimal = parseInt(input.value.trim(), 10);
-  const binary = decimalToBinary(decimal);
-  resultDiv.textContent = binary;
-});
-
-function decimalToBinary(decimal) {
-  let binary = '';
-  while (decimal > 0) {
-    binary = (decimal % 2) + binary;
-    decimal = Math.floor(decimal / 2);
-  }
-  return binary || '0';
+function decimalToBinary(num) {
+  // Write your code here
 }
 
-module.exports = threeSum;
+window.decimalToBinary = decimalToBinary;
