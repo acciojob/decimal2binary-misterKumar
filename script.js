303136812bc024e7080bba1e42a518e17b1316ca
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-14 15:21:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function decimalToBinary(num) {
-  // Write your code here
+  //Write you code here
+  if (num < 0) {
+        return "Please enter a non-negative decimal number.";
+    }
+
+    if (num === 0) {
+        return "0";
+    }
+
+    let binaryNum = "";
+    while (num > 0) {
+        binaryNum = (num % 2) + binaryNum;
+        num = Math.floor(num / 2);
+    }
+
+    return binaryNum;
 }
 
 window.decimalToBinary = decimalToBinary;
